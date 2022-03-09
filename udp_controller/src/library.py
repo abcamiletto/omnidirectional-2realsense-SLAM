69729f836024ec715a3be8c88e83bd5180c2a18f
ltiseni94 <luca13tiseni@gmail.com>
2022-03-09 12:25:21 +0100

    debug mod

--- udp_controller/src/library.py
+++ udp_controller/src/library.py
@@ -5,5 +5,6 @@ def receive_data(sock, length=512, timeout=0.00005):
     while True:
         read_fds, _, _ = select([sock], [], [], timeout)
         if sock in read_fds:
+            print('Incoming data')
             data, _ = sock.recvfrom(length)
             return data
